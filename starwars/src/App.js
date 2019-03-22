f37884f483d33aee4a2f8ae0328c9356dffd29f1
cammac <camm@rocktechcomputer.com>
2019-03-22 08:24:11 -0700

    added and rendered list component

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+
+import CharacterList from './components/CharacterList';
 import './App.css';
 
 class App extends Component {
@@ -33,7 +35,10 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+
+        <CharacterList />
       </div>
+
     );
   }
 }
---
+++ starwars/src/components/CharacterList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CharacterList = props => {
+  return (
+    <div>
+      <h2>List of Characters</h2>
+    </div>
+  )
+}
+
+export default CharacterList;
